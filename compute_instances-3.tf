ac1a60c5c00280efc8e7e5f9538bea70583c896c
fatihtokus <fatih.tokus@gmail.com>
2023-04-27 16:36:26 +0100

    wip

--- compute_instances-3.tf
+++ compute_instances-3.tf
@@ -1,6 +1,6 @@
 resource "google_compute_instance" "default" {
-  name         = "test" - 3
-  machine_type = "e2-medium"
+  name         = "test-3"
+  machine_type = "e2-small"
   zone         = "us-central1-a"
 
 
