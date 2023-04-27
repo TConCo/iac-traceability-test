e5469e587f3903bd6e950b4366820061639b1a57
fatihtokus <fatih.tokus@gmail.com>
2023-04-27 16:58:13 +0100

    wip

--- compute_instances-3.tf
+++ compute_instances-3.tf
@@ -5,7 +5,9 @@ resource "google_compute_instance" "default" {
 
 
   tags = ["foo", "bar"]
-  labels = {
-    yor_trace = "df432164-7951-42a7-9679-f09d172a476c"
-  }
+}
+
+
+resource "aws_vpc" "vpc_tags_one_line" {
+  cidr_block = ""
 }
