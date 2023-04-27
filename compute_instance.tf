55b06a8f417f1ca02c285ac40b54828214fab26e
Fatih Tokus <fatih.tokus@gmail.com>
2023-04-27 14:36:26 +0100

    Update compute_instance.tf

--- compute_instance.tf
+++ compute_instance.tf
@@ -4,6 +4,12 @@ resource "google_service_account" "default" {
 }
 
 resource "google_compute_instance" "default" {
+  name         = "test-2"
+  machine_type = "e2-medium"
+  zone         = "us-central1-a"
+}
+
+resource "google_compute_instance" "default" {
   name         = "test"
   machine_type = "e2-medium"
   zone         = "us-central1-a"
